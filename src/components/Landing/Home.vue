14393dca218b7389902e50a95800a13f88ddee2d
alejandro.c26 <castanojo21@gmail.com>
2022-09-03 11:58:24 -0500

    Se quedaron estos archivos

---
+++ src/components/Landing/Home.vue
@@ -0,0 +1,72 @@
+<template>
+
+    <body class="body_home">
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Tienda</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto ">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Inicia</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Registrate</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Tiendas</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Comidas rápidas</a></li>
+                            <li><a class="dropdown-item" href="#">Farmacias</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><img class="carrito" src="https://cdn-icons-png.flaticon.com/512/107/107831.png" ></a>
+                        </li>
+                    </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+        </nav>
+
+<div class="masterhome" style="background-image:url('./fondo.png')">
+        <div class="color-overlay d-flex justify-content-center align-items-center">
+            <section class="home">
+                <div class="container">
+                    <div class="left">
+                    <h2 class="left_h2">TU RESTAURANTE FAVORITO EN CALI A LA HORA QUE QUIERAS</h2>
+                    <p class="parrafo_left">Ordena cuando y desde donde quieras</p>
+                    </div>
+                        <div class="right">
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+
+
+    </body>
+
+    <footer>
+
+    </footer>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Landing/Shops.vue
@@ -0,0 +1,155 @@
+<template>
+        <header>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Tienda</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto ">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Inicia</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Registrate</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Tiendas</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Comidas rápidas</a></li>
+                            <li><a class="dropdown-item" href="#">Farmacias</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><img class="carrito" src="https://cdn-icons-png.flaticon.com/512/107/107831.png" ></a>
+                        </li>
+                    </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                </div>
+            </div>
+            </nav>
+        </header>
+    
+
+<body class="p-3 m-1 border-1 bd-example bd-example-row">
+
+<!-- Example Code -->
+
+<div class="container">
+<div class="row">
+<div class="col-6 col-sm-3" style="padding-left:110px">
+<div class="card" style="width: 18rem;">
+<svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+
+<div class="card-body">
+  <h5 class="card-title">Negocio 1</h5>
+  <p class="card-text">This is a general description</p>
+  <a href="#" class="btn btn-warning">Leer más</a>
+</div>
+</div>
+
+  </div>
+  <div class="col-6 col-sm-3" style="padding-left:120px">
+  <div class="card" style="width: 18rem;">
+  <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+  
+  <div class="card-body">
+    <h5 class="card-title">Negocio 2</h5>
+    <p class="card-text">This is a general description</p>
+    <a href="#" class="btn btn-warning">Leer más</a>
+  </div>
+</div>
+  </div>
+
+  <div class="col-6 col-sm-3" style="padding-left:130px">
+  <div class="card" style="width: 18rem;">
+  <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+  
+  <div class="card-body">
+    <h5 class="card-title">Negocio 3</h5>
+    <p class="card-text">This is a general description</p>
+    <a href="#" class="btn btn-warning">Leer más</a>
+  </div>
+</div>
+  </div>
+
+  <!-- Force next columns to break to new line -->
+  <div class="w-100"></div>
+
+
+  
+  <div class="col-6 col-sm-3" style="padding-left:110px">
+  <div class="card" style="width: 18rem;">
+  <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+  
+  <div class="card-body">
+    <h5 class="card-title">Negocio 4</h5>
+    <p class="card-text">This is a general description.</p>
+    <a href="#" class="btn btn-warning">Leer más</a>
+  </div>
+</div>
+  </div>
+
+  <div class="col-6 col-sm-3" style="padding-left: 120px">
+  <div class="card" style="width: 18rem;">
+  <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+  
+  <div class="card-body">
+    <h5 class="card-title">Negocio 5</h5>
+    <p class="card-text">This is a general description.</p>
+    <a href="#" class="btn btn-warning">Leer más</a>
+  </div>
+</div>
+  </div>
+
+  <div class="col-6 col-sm-3" style="padding-left: 130px">
+  <div class="card" style="width: 18rem;">
+  <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+  
+  <div class="card-body">
+    <h5 class="card-title">Negocio 6</h5>
+    <p class="card-text">This is a general description.</p>
+    <a href="#" class="btn btn-warning">Leer más</a>
+  </div>
+</div>
+  </div>
+</div>
+</div>
+
+<footer class="bg-dark text-white">
+    <div class="container">
+        <nav class="row">
+            <a href="#" class="col-3 text-reset text uppercase d-flex align-items-center">
+                <p>​© 2022 Ambiente en desarrollo web Inc. All rights reserved.</p>
+            </a>
+            
+            <ul class="col-3 list unstyled">
+                <li class="font-weight-bold text-uppercase">Redes sociales</li>
+                
+                    <a href="#" class="text-reset">
+                        <i class="fa-brands fa-facebook"></i>
+                    </a>
+                    <a href="#" class="text-reset">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="#" class="text-reset">
+                        <i class="fa-brands fa-twitter"></i>
+                    </a>
+                
+            </ul>
+        </nav>
+        
+    </div>
+</footer>
+
+
+<!-- End Example Code -->
+
+    </body>
+</template>
