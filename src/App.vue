4ca68b831a94ec9581287a63c69a28e74c202707
alejandro.c26 <castanojo21@gmail.com>
2022-09-06 18:19:48 -0500

    App.vue modified

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <template>
-  <shops/>
+  <RouterView/>
 </template>
 
 
 <script>
 
-import Home from './components/Landing/Home.vue';
-import Login from './components/Login/Login.vue';
-import Register from './components/Register/Register.vue';
-import Shops from './components/Shop/Shops.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Home,
-    Login,
-    Register,
-    Shops
-  }
-}
 </script>
+
+<style scoped>
+
+</style>
 
